f9ad7de4646a959f2cb1cd9488dca63d5737742a
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-01-08 16:52:48 +0100

    create a function that takes in positional and keyword arguments to raise a number to the power of the keyword argument

--- oneoffs.py
+++ oneoffs.py
@@ -5,3 +5,10 @@ def divide(num1, num2, floor = True):
         return num1 // num2
     except TypeError:
         return 'Invalid Input Type. Please make sure that you enter a valid number'
+
+
+def raise_to_the_power_of(number, *, power):
+    try:
+        return number ** power
+    except TypeError:
+        return 'Invalid input type. Ensure that you enter valid numbers'
